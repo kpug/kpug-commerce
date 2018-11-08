5d52a3e1d00de3a8a652a6935ab1ed2a9c3686b3
Joseph Yang <before30@gmail.com>
2018-11-09 00:21:41 +0900

    Add Cloud Config Server

--- infra-cloud-config/src/main/java/com/kpug/infra/cloudconfig/CloudConfigServerApplication.java
+++ infra-cloud-config/src/main/java/com/kpug/infra/cloudconfig/CloudConfigServerApplication.java
@@ -1,7 +1,15 @@
 package com.kpug.infra.cloudconfig;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class CloudConfigServerApplication {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        SpringApplication.run(CloudConfigServerApplication.class, args);
     }
 }
