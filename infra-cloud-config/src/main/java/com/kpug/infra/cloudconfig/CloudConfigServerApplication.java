438bcf360825e910ecc999fa632a417c31a44d7c
Joseph Yang <before30@gmail.com>
2018-11-08 16:35:32 +0900

    Add hystrix dashboard and cloud config

---
+++ infra-cloud-config/src/main/java/com/kpug/infra/cloudconfig/CloudConfigServerApplication.java
@@ -0,0 +1,7 @@
+package com.kpug.infra.cloudconfig;
+
+public class CloudConfigServerApplication {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
